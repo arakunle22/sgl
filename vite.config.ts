e9ff4549e8b54736ac939441cd2b7021913e6646
arakunle22 <103513961+arakunle22@users.noreply.github.com>
2025-01-19 02:33:06 +0100

    configure Vite server settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Expose the server to all network interfaces
+    port: process.env.PORT ? parseInt(process.env.PORT as string, 10) : 3000, // Cast string to number
+  },
 })
